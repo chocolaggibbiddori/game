1f42b55918804138b5ccdba785976c0832e99814
chocolaggibbiddori <didrkd684@naver.com>
2022-12-14 15:45:25 +0900

    feat : Point 생성

---
+++ src/main/java/mygame/point/Point.java
@@ -0,0 +1,35 @@
+package mygame.point;
+
+public class Point {
+
+    int x;
+    int y;
+    String strPoint;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+        strPoint = setStrPoint(x, y);
+    }
+
+    public Point(String strPoint) {
+        String lowerStrPoint = strPoint.toLowerCase();
+        this.strPoint = lowerStrPoint;
+        x = setX(lowerStrPoint);
+        y = setY(lowerStrPoint);
+    }
+
+    private String setStrPoint(int x, int y) {
+        String before = String.valueOf(8 - x);
+        String after = String.valueOf('a' + y);
+        return after + before;
+    }
+
+    private int setX(String strPoint) {
+        return '8' - strPoint.charAt(1);
+    }
+
+    private int setY(String strPoint) {
+        return strPoint.charAt(0) - 'a';
+    }
+}
