3dea54435e5f958baa9e3293711acd5005a871a1
chocolaggibbiddori <didrkd684@naver.com>
2022-12-17 19:31:59 +0900

    feat : ChessNotationRepository 생성

---
+++ src/main/java/mygame/game/chess/repository/ChessNotationRepository.java
@@ -0,0 +1,17 @@
+package mygame.game.chess.repository;
+
+import mygame.game.chess.turn.ChessNotation;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class ChessNotationRepository {
+
+    private static final List<ChessNotation> notationList = new ArrayList<>();
+
+    public void save(int turnNumber, ChessNotation notation) {
+        notationList.add(turnNumber, notation);
+    }
+}
