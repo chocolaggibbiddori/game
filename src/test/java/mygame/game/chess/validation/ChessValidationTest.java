efbca7c128cce98c6bae3be787d13ecab93a42a9
chocolaggibbiddori <didrkd684@naver.com>
2022-12-24 12:48:29 +0900

    test : Rook moveList 테스트

---
+++ src/test/java/mygame/game/chess/validation/ChessValidationTest.java
@@ -0,0 +1,41 @@
+package mygame.game.chess.validation;
+
+import mygame.game.chess.board.ChessBoard;
+import mygame.game.chess.name.Name;
+import mygame.game.chess.piece.Pawn;
+import mygame.game.chess.piece.Rook;
+import mygame.game.chess.point.ChessPoint;
+import mygame.game.chess.repository.ChessNotationRepository;
+import mygame.game.chess.turn.ChessTurn;
+import mygame.piece.Piece;
+import mygame.point.Point;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ChessValidationTest {
+
+    ChessBoard board = new ChessBoard();
+    ChessNotationRepository repository = new ChessNotationRepository();
+    ChessTurn turn = new ChessTurn(repository);
+    ChessValidation validation = new ChessValidation(board, turn, repository);
+
+    @Test
+    void moveList() {
+        ChessPoint point = new ChessPoint("a1");
+        Rook rook = new Rook(point, Name.TEAM_WHITE);
+
+        board.setInBoard(rook, point);
+        Piece piece = board.findByPoint(point);
+        assertThat(piece).isEqualTo(rook);
+
+        ChessPoint point2 = new ChessPoint("d4");
+        Pawn pawn = new Pawn(point2, Name.TEAM_BLACK);
+        board.setInBoard(pawn, point2);
+
+        List<Point> moveList = validation.moveList(rook);
+        System.out.println(moveList);
+    }
+}
