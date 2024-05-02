f8eb4de59b45a0bb5027839e5992d8f60edf7212
chocolaggibbiddori <115144342+chocolaggibbiddori@users.noreply.github.com>
2024-05-02 23:27:36 +0900

    Add: [chess module] Knight 클래스 초기 작성

---
+++ chess/src/java/chocola/chess/piece/Knight.java
@@ -0,0 +1,39 @@
+package chocola.chess.piece;
+
+import chocola.chess.Team;
+import chocola.chess.Tile;
+
+public class Knight extends Piece {
+
+    public Knight(Team team) {
+        super(team);
+    }
+
+    public boolean canMoveTo(Tile to) {
+        Tile position = getPosition();
+        int pFile = position.getFileIdx();
+        int pRank = position.getRankIdx();
+        int tFile = to.getFileIdx();
+        int tRank = to.getRankIdx();
+
+        return is3DiffTile(pFile, pRank, tFile, tRank) &&
+               isNotSame(pFile, tFile) &&
+               isNotSame(pRank, tRank);
+    }
+
+    private boolean is3DiffTile(int pFile, int pRank, int tFile, int tRank) {
+        return absOfDiff(pFile, tFile) + absOfDiff(pRank, tRank) == 3;
+    }
+
+    private int absOfDiff(int pFile, int tFile) {
+        return Math.abs(pFile - tFile);
+    }
+
+    private boolean isNotSame(int i1, int i2) {
+        return i1 != i2;
+    }
+
+    public char getInitial() {
+        return 'N';
+    }
+}
