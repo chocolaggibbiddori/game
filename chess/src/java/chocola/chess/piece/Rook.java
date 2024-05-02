6d4ea691e6bb40e7b575490eec9374e9b0128a5a
chocolaggibbiddori <115144342+chocolaggibbiddori@users.noreply.github.com>
2024-05-02 23:37:34 +0900

    Add: [chess module] Rook 클래스 초기 작성

---
+++ chess/src/java/chocola/chess/piece/Rook.java
@@ -0,0 +1,27 @@
+package chocola.chess.piece;
+
+import chocola.chess.Team;
+import chocola.chess.Tile;
+
+public final class Rook extends MoveCheckedPiece {
+
+    public Rook(Team team) {
+        super(team);
+    }
+
+    @Override
+    public boolean canMoveTo(Tile to) {
+        Tile position = getPosition();
+        int pFile = position.getFileIdx();
+        int pRank = position.getRankIdx();
+        int tFile = to.getFileIdx();
+        int tRank = to.getRankIdx();
+
+        return pFile == tFile || pRank == tRank;
+    }
+
+    @Override
+    public char getInitial() {
+        return 'R';
+    }
+}
