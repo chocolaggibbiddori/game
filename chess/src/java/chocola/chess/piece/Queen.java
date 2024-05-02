f6bd95f969e8bf14ba4f1a641b3de7d5ce2f1203
chocolaggibbiddori <115144342+chocolaggibbiddori@users.noreply.github.com>
2024-05-02 23:33:54 +0900

    Add: [chess module] Queen 클래스 초기 작성

---
+++ chess/src/java/chocola/chess/piece/Queen.java
@@ -0,0 +1,37 @@
+package chocola.chess.piece;
+
+import chocola.chess.Team;
+import chocola.chess.Tile;
+
+public class Queen extends Piece {
+
+    public Queen(Team team) {
+        super(team);
+    }
+
+    public boolean canMoveTo(Tile to) {
+        Tile position = getPosition();
+        int pFile = position.getFileIdx();
+        int pRank = position.getRankIdx();
+        int tFile = to.getFileIdx();
+        int tRank = to.getRankIdx();
+
+        return isStraight(pFile, pRank, tFile, tRank) || isDiagonal(pFile, pRank, tFile, tRank);
+    }
+
+    private boolean isStraight(int pFile, int pRank, int tFile, int tRank) {
+        return pFile == tFile || pRank == tRank;
+    }
+
+    private boolean isDiagonal(int pFile, int pRank, int tFile, int tRank) {
+        return absOfDiff(pFile, tFile) == absOfDiff(pRank, tRank);
+    }
+
+    private int absOfDiff(int i1, int i2) {
+        return Math.abs(i1 - i2);
+    }
+
+    public char getInitial() {
+        return 'Q';
+    }
+}
