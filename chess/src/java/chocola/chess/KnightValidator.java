afd64e8ca3440f735ebf7fc0ee5d7d7b4dc047a5
chocolaggibbiddori <115144342+chocolaggibbiddori@users.noreply.github.com>
2024-05-06 14:05:55 +0900

    Fix: [chess module] KnightValidator - 타입 체크 오류 수정

--- chess/src/java/chocola/chess/KnightValidator.java
+++ chess/src/java/chocola/chess/KnightValidator.java
@@ -7,7 +7,7 @@ class KnightValidator implements Validator {
 
     @Override
     public boolean isValid(ChessBoard chessBoard, Piece piece, Tile to) {
-        if (piece instanceof Knight) throw new IllegalArgumentException();
+        if (!(piece instanceof Knight)) throw new IllegalArgumentException();
         return true;
     }
 }
