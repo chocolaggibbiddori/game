ec7e3a78599bc42a1c144815b53ec74e93322038
chocolaggibbiddori <115144342+chocolaggibbiddori@users.noreply.github.com>
2024-05-06 13:42:13 +0900

    Add: [chess module] QueenValidator 클래스 초기 작성

---
+++ chess/src/java/chocola/chess/QueenValidator.java
@@ -0,0 +1,12 @@
+package chocola.chess;
+
+import chocola.chess.piece.Piece;
+import chocola.chess.piece.Queen;
+
+class QueenValidator extends StraightPieceValidator {
+
+    @Override
+    void checkType(Piece piece) {
+        if (piece instanceof Queen) throw new IllegalArgumentException();
+    }
+}
